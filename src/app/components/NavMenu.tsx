a458045813725ad9ec9775d57ba37487997a09a4
pawanbenjamin <pawanbenjamin@gmail.com>
2024-01-23 11:44:08 -0600

    protected route styling

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -1,6 +1,10 @@
 "use client";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
+
+const ACTIVE_ROUTE = "bg-blue-300";
+const INACTIVE_ROUTE = "";
 
 function AuthButton() {
   const { data: session } = useSession();
@@ -23,9 +27,27 @@ function AuthButton() {
 }
 
 export default function NavMenu() {
+  const pathname = usePathname();
   return (
     <div>
       <AuthButton />
+      <hr />
+      <ul>
+        <Link href="/">
+          <li className={pathname === "/" ? ACTIVE_ROUTE : INACTIVE_ROUTE}>
+            Home
+          </li>
+        </Link>
+        <Link href="/protected">
+          <li
+            className={
+              pathname === "/protected" ? ACTIVE_ROUTE : INACTIVE_ROUTE
+            }
+          >
+            Protected Route
+          </li>
+        </Link>
+      </ul>
     </div>
   );
 }
